422537e59432bc002f7dcfc964c82dfeeed782fc
David Nicklas <david.nicklas@me.com>
2025-05-15 10:03:23 +0200

    Setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 ﻿#include <cstdlib>
 
 #include "raylib.h"
-
 #include "config.h"
 
 int main() {
-    // Raylib initialization
     // Project name, screen size, fullscreen mode etc. can be specified in the config.h.in file
     InitWindow(Game::ScreenWidth, Game::ScreenHeight, Game::PROJECT_NAME);
     SetTargetFPS(60);
@@ -15,32 +13,21 @@ int main() {
 #endif
 
     // Your own initialization code here
-    // ...
-    // ...
-    Texture2D myTexture = LoadTexture("assets/graphics/testimage.png");
 
     // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Updates that are made by frame are coded here
-        // ...
-        // ...
+        // Game Logic Update
 
+        // Drawing
         BeginDrawing();
-            // You can draw on the screen between BeginDrawing() and EndDrawing()
-            // ...
-            // ...
             ClearBackground(WHITE);
             DrawText("Hello, world!", 10, 10, 30, LIGHTGRAY);
-            DrawTexture(myTexture, 10, 100, WHITE);
 
         EndDrawing();
     } // Main game loop end
 
     // De-initialization here
-    // ...
-    // ...
-    UnloadTexture(myTexture);
 
     // Close window and OpenGL context
     CloseWindow();
