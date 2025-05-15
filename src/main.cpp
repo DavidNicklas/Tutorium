04296941cba6e59eab68c323b5674dd64166d4b9
David Nicklas <david.nicklas@me.com>
2025-05-15 11:47:17 +0200

    map drawing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 ﻿#include <cstdlib>
 
 #include "raylib.h"
+#include <fstream>
+#include <iostream>
 #include "config.h"
+#include "../libs/json.hpp"
+
+using json = nlohmann::json;
 
 int main() {
     // Project name, screen size, fullscreen mode etc. can be specified in the config.h.in file
@@ -13,6 +18,14 @@ int main() {
 #endif
 
     // Your own initialization code here
+    std::ifstream file("assets/maps/TutoriumSampleMap.json");
+    json loadedJsonMap;
+    file >> loadedJsonMap; // Read the file into the json object using >> overloaded operator
+
+    Texture2D tileSet = LoadTexture("assets/graphics/tilesets/Tileset.png");
+
+    const int tileSize = 16;
+    const int tileSetColumns = 27;
 
     // Main game loop
     while (!WindowShouldClose())
@@ -22,12 +35,42 @@ int main() {
         // Drawing
         BeginDrawing();
             ClearBackground(WHITE);
-            DrawText("Hello, world!", 10, 10, 30, LIGHTGRAY);
+
+            auto layerCount = loadedJsonMap["layers"].size();
+            for (int i = 0; i < layerCount; ++i)
+            {
+                auto layer = loadedJsonMap["layers"][i];
+                int mapWidth = layer["width"];
+                int mapHeight = layer["height"];
+                // Draw the loaded map
+                for (int y = 0; y < mapHeight; y++)
+                {
+                    for (int x = 0; x < mapWidth; x++)
+                    {
+                        int index = y * mapWidth + x;
+                        int tiledID = layer["data"][index];
+                        if (tiledID == 0) continue; // Skip empty tiles
+
+                        int tiledIndex = tiledID - 1;
+                        Rectangle srcRect = {
+                        static_cast<float>((tiledIndex % tileSetColumns) * tileSize), // Assuming tile size is 32x32
+                            static_cast<float>((tiledIndex / tileSetColumns) * tileSize),
+                            (float)tileSize, (float)tileSize
+                        };
+
+                        Vector2 pos = { (float)(x * tileSize) * 1.0f, (float)(y * tileSize) * 1.0f };
+                        // Draw the tile
+                        DrawTextureRec(tileSet, srcRect, pos, WHITE);
+                    }
+                }
+            }
+
 
         EndDrawing();
     } // Main game loop end
 
     // De-initialization here
+    UnloadTexture(tileSet);
 
     // Close window and OpenGL context
     CloseWindow();
